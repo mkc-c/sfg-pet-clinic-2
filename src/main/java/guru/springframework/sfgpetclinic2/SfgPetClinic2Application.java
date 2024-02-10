b2b4b57340b65554c205b4027959f515f848d925
mkc-c <abdirisak.arr@gmail.com>
2024-02-10 12:05:43 +0000

    reinitialize project

---
+++ src/main/java/guru/springframework/sfgpetclinic2/SfgPetClinic2Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinic2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClinic2Application.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sfgpetclinic2/SfgPetClinic2ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinic2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
