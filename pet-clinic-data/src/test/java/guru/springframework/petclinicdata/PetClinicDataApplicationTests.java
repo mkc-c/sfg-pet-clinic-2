6a7701c265fec83bee2928d200e8e3e235600fc3
mkc-c <abdirisak.arr@gmail.com>
2024-02-13 13:34:27 +0000

    re-multimodulerise spring app

--- pet-clinic-data/src/test/java/guru/springframework/petclinicdata/PetClinicDataApplicationTests.java
+++ pet-clinic-data/src/test/java/guru/springframework/petclinicdata/PetClinicDataApplicationTests.java
@@ -1,10 +1,10 @@
-package guru.springframework.sfgpetclinic2;
+package guru.springframework.petclinicdata;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = {SfgPetClinic2Application.class})
-class SfgPetClinic2ApplicationTests {
+@SpringBootTest
+class PetClinicDataApplicationTests {
 
     @Test
     void contextLoads() {
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic2/SfgPetClinic2Application.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic2/SfgPetClinic2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SfgPetClinic2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SfgPetClinic2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinic2Application.class, args);
+    }
 
 }
