ee19e9c67b231c174c27636f92b50bfc7b22cbf8
mkc-c <abdirisak.arr@gmail.com>
2024-02-13 14:24:17 +0000

    adding base entity. Closes #10

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic2/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic2/model/Person.java
@@ -1,6 +1,6 @@
 package guru.springframework.sfgpetclinic2.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic2/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic2/model/PetType.java
@@ -1,6 +1,6 @@
 package guru.springframework.sfgpetclinic2.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
