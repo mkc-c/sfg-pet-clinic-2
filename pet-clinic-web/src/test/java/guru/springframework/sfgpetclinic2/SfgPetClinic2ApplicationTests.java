3716f588509f7410f4cedb886d4cdb0fe1c1cbd4
mkc-c <abdirisak.arr@gmail.com>
2024-02-13 09:38:59 +0000

    refactor to multimodule maven project. Closes #1

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic2/SfgPetClinic2ApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic2/SfgPetClinic2ApplicationTests.java
@@ -3,11 +3,11 @@ package guru.springframework.sfgpetclinic2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {SfgPetClinic2Application.class})
 class SfgPetClinic2ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
